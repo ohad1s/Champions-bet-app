2cd1413d932ebf62fe86f17be29d4889dbab7da8
shirazi97 <shirazi1997@gmail.com>
2022-11-20 13:20:08 +0200

    java classes

--- app/src/main/java/src/games/Group.java
+++ app/src/main/java/src/games/Group.java
@@ -1,6 +1,17 @@
 package src.games;
-import src.participants.User
+import java.util.ArrayList;
+import src.participants.Manager;
+import src.participants.User;
 
 public class Group {
-    private User[]users;
+    private ArrayList<User> users ;
+    private String code;
+    private Manager manager;
+
+    public Group(String code,Manager manager){
+        this.code=code;
+        this.manager=manager;
+        this.users= new ArrayList<User>();
+        users.add(manager);
+    }
 }
---
+++ app/src/main/java/src/participants/Player.java
@@ -0,0 +1,7 @@
+package src.participants;
+
+public class Player extends User{
+    protected Player(String email, String password, String nickname) {
+        super(email, password, nickname);
+    }
+}
