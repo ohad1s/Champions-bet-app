ed03a927864dd96c7057b46353ecbb1d0209ab09
shirazi97 <shirazi1997@gmail.com>
2022-11-20 14:35:02 +0200

    bet

--- app/src/main/java/src/games/Game.java
+++ app/src/main/java/src/games/Game.java
@@ -1,21 +1,26 @@
 package src.games;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Game {
+    int id;
     String home;
     String away;
     int home_score;
     int away_score;
     int points;
+    Date final_date;
     ArrayList<Bet>bets;
 
-    public Game(String home, String away, int p){
+    public Game(int id,String home, String away, int p, Date fd){
+        this.id =id;
         this.home=home;
         this.away=away;
         this.points=p;
         this.away_score=-1;
         this.home_score=-1;
+        this.final_date=fd;
         bets= new ArrayList<Bet>();
     }
 
