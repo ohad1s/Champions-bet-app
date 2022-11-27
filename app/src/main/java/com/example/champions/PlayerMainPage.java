6b06058b13bd824be4fd6b900fed1186530e2e48
dvirGev <dvir87879@gmail.com>
2022-11-27 13:43:45 +0200

    my commit :)

---
+++ app/src/main/java/com/example/champions/PlayerMainPage.java
@@ -0,0 +1,14 @@
+package com.example.champions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_main_page);
+    }
+}
