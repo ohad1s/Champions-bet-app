4d7044e1c45de6c3f7f2eddaaa17110198c946e3
dvirBiton1 <dvir199710@gmail.com>
2022-12-04 14:37:32 +0200

    player login

--- app/src/main/java/com/example/champions/ManagerRegisterActivity.java
+++ app/src/main/java/com/example/champions/ManagerRegisterActivity.java
@@ -15,32 +15,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.sql.Time;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
-import kotlin.jvm.internal.Ref;
-import src.games.Bet;
-import src.games.Game;
-import src.games.Team;
 import src.games.User;
 
 public class ManagerRegisterActivity extends AppCompatActivity {
@@ -73,11 +53,11 @@ public class ManagerRegisterActivity extends AppCompatActivity {
                 } else if (text_password.length() < 6) {
                     Toast.makeText(ManagerRegisterActivity.this, "Password too short", Toast.LENGTH_SHORT).show();
                 } else {
+                    System.out.println("dsfdsfdsfdsfsdf");
                     registerUser(text_email, text_password);
-                    addUserToDB(text_email, text_password, "itamar");
-                    Intent toManagerLogin = new Intent(ManagerRegisterActivity.this, MangerLoginActivity.class);
-                    startActivity(toManagerLogin);
-                    finish();
+//                    Intent toManagerLogin = new Intent(ManagerRegisterActivity.this, MangerLoginActivity.class);
+//                    startActivity(toManagerLogin);
+//                    finish();
                 }
             }
         });
@@ -88,8 +68,13 @@ public class ManagerRegisterActivity extends AppCompatActivity {
     private void registerUser(String text_email, String text_password) {
         auth.createUserWithEmailAndPassword(text_email, text_password).addOnCompleteListener(   this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onComplete(Task<AuthResult> task) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = auth.getCurrentUser();
+                    String userid =  auth.getUid();
+                    Log.d(TAG, auth.getUid());
+                    addUserToDB(userid, text_email, text_password, "dvirr");
                     Toast.makeText(ManagerRegisterActivity.this, "Register successful!", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(ManagerRegisterActivity.this, "Register failed!", Toast.LENGTH_SHORT).show();
@@ -97,22 +82,14 @@ public class ManagerRegisterActivity extends AppCompatActivity {
                 }
             }
         });
-//        String userid = FirebaseAuth.getInstance().getCurrentUser().toString();
-//        return userid;
+
     }
-    public void addUserToDB(String email, String password, String nickname) {
-        FirebaseUser userid = FirebaseAuth.getInstance().updateCurrentUser();
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        User user = new User(userid.getUid(), email, password, nickname);
+    public void addUserToDB(String userid, String email, String password, String nickname) {
+        User user = new User(userid, email, password, nickname);
         firebaseDatabase.collection("users").document(user.getUserID()).set(user.toHashMap()).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(ManagerRegisterActivity.this, "add succees", Toast.LENGTH_SHORT).show();
                 }
             }
