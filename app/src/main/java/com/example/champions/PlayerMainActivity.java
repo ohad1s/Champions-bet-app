a880b2b18961a5e8179fd42f4e076ed66e898da9
shirazi97 <shirazi1997@gmail.com>
2022-12-17 18:35:15 +0200

    betting on game- need to fix save button

--- app/src/main/java/com/example/champions/PlayerMainActivity.java
+++ app/src/main/java/com/example/champions/PlayerMainActivity.java
@@ -86,11 +86,12 @@ public class PlayerMainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.i("COSTUM_LIST_VIEW", "Item is clicked @ i :: " + i );
-                Intent intent = new Intent(PlayerMainActivity.this, game_list_view_player.class);
+                Intent intent = new Intent(PlayerMainActivity.this, tournament_user_page.class);
                 Bundle b = new Bundle();
                 b.putString("userid", user.getUserID());
                 b.putString("tournamentid", user_tournament.get(i).getTournamentID()); //tournament id
                 b.putInt("tournamentIndex", i);
+                b.putString("token", user_tournament.get(i).getTournamentID());
                 intent.putExtras(b); //Put your id to your next Intent
                 startActivity(intent);
                 finish();
